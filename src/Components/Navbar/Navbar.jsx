c88ee02977250f9a2e402f4ab69fdddfdfdef3e2
pawan rajora <pawanrajora143@gmail.com>
2024-07-10 20:23:09 +0530

    add

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,24 @@
-import React, { useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import "./Navbar.css"
 import logo from "../Assets/logo.png"
 import cart_icon from "../Assets/cart_icon.png"
 import { Link } from 'react-router-dom'
+import { ShopContext } from '../../Context/ShopContext'
+import nav_dropdown from "../Assets/nav_dropdown.png"
 
 
 const Navbar = () => {
 
     const [menu,setMenu] = useState("shop");
+    const {getTotalCartItems} = useContext(ShopContext)
+    const menuRef = useRef();
 
 
+    const dropdown_toggle = (e) => {
+        menuRef.current.classList.toggle("nav-menu-visible")
+        e.target.classList.toggle("open")
+
+    }
 
   return (
 
@@ -18,7 +27,8 @@ const Navbar = () => {
         <img src={logo} alt="" />
         <p>Shopper</p>
       </div>
-      <ul className='nav-menu'>
+      <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+      <ul ref={menuRef} className='nav-menu'>
         <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration:"none"}} to="/">Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
         <li onClick={()=>{setMenu("mens")}}><Link style={{textDecoration:"none"}}  to="/mens">Mens</Link>{menu==="mens"?<hr/>:<></>}</li>
         <li onClick={()=>{setMenu("womens")}}><Link style={{textDecoration:"none"}} to="/womens">Womens</Link>{menu==="womens"?<hr/>:<></>}</li>
@@ -27,7 +37,7 @@ const Navbar = () => {
       <div className="nav-login-cart">
         <Link to="/login"><button>Login</button></Link>
         <Link to="/cart"><img src={cart_icon} alt="" /></Link>
-        <div className="nav-cart-count">0</div>
+        <div className="nav-cart-count">{getTotalCartItems()}</div>
       </div>
     </div>
   )
--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -44,11 +44,24 @@ const ShopContextProvider = (props) => {
 }
   }
 
+
+  const getTotalCartItems = () => {
+    let totalItem = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        totalItem += cartItems[item];
+      }
+    }
+    return totalItem;
+  }
+
   const contextValue = {
+    getTotalCartItems,
+    getTotalCartAmount,
     all_product,
     cartItems,
     addToCart,
-    removeFromCart,
+    removeFromCart
   };
 
   return (
